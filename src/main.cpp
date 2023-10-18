e8494645627b80086ed81cb9ac2fa5dade8e7e43
jojo313 <32206115+jojo313@users.noreply.github.com>
2023-10-18 18:20:30 +0200

    Korrekte Abtastung (code in interrupt, nicht loop)
    Co-authored-by: suta1011 <suta1011@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ Beschreibung: Main
 //#include <driver/adc.h>
 //#include <driver/dac.h>
 
-SSD1306Wire display(0x3c, SDA, SCL);  // ADDRESS, SDA, SCL | 128x64 display
+SSD1306Wire display(0x3c, SDA, SCL);  // ADDRESS, SDA, SCL | 128x64 display https://github.com/ThingPulse/esp8266-oled-ssd1306
 
 // ADC-Konfiguration
 #define ADC_PIN 33 // Pin für EKG-Signal
@@ -40,11 +40,20 @@ float rxvoltage;
 int16_t xdisplay = 0;
 int16_t  ydisplay = 32;
 
-int delayvalue = 0;
+struct myPixel                              // Pixels for use with Display
+{
+  int16_t x = 0;
+  int16_t y = 0;
+};
+
+myPixel lastpx;
+myPixel displaypx;
+
+int delayvalue = 0;                          // Index wie viel Werte für die Ausgabe übersprungen werden
 
 // Werte festlegen
-int samplingFreq = 250;       // Abtastfrequenz ADC https://pubmed.ncbi.nlm.nih.gov/30109153/
-int displayTimeShown = 2;     // Auf Display werden 2s an Daten angezeigt
+int samplingFreq = 250;                      // Abtastfrequenz ADC https://pubmed.ncbi.nlm.nih.gov/30109153/
+int displayTimeShown = 2;                    // Auf Display werden 2s an Daten angezeigt
 
 // Variablen Definition
 std::queue<uint16_t> ringBuffer;
@@ -66,7 +75,13 @@ void IRAM_ATTR onTimer()                    // Interrupt Ram Attritube
   //int16_t adcValue = analogRead(ADC_PIN);
   //ekgBuffer[bufferIndex] = adcValue;
   //bufferIndex = (bufferIndex + 1) % BUFFER_SIZE;
+  
   interruptflag = true;
+
+  digitalWrite(26, HIGH);                  // sets the digital pin 26 on
+  ringBuffer.push(analogRead(ADC_PIN));     // Read analog signal and write to buffer
+  interruptflag = false;                    // Reset interrupt flag
+  digitalWrite(26, LOW);                   // sets the digital pin 26 off
 
   // Hier können Sie Code hinzufügen, um Daten auf GPIO 25 zu schreiben (WRITE_PIN).
   // Zum Beispiel digitalWrite(WRITE_PIN, HIGH) für eine bestimmte Bedingung.
@@ -76,12 +91,18 @@ void setup()
 {
   Serial.begin(115200);
 
+
+  // Initialisierung PINS
+  //pinMode(33, INPUT);     // sets the digital pin 33 as input
+  //pinMode(25, OUTPUT);    // sets the digital pin 25 as output
+  pinMode(26, OUTPUT);    // sets the digital pin 26 as output
+
   // Display initialisieren
   display.init();
   display.clear();
   display.display(); 
 
-  // Timer                                                              // https://github.com/pcbreflux/espressif/blob/master/esp32/arduino/sketchbook/ESP32_simpletimer/ESP32_simpletimer.ino
+  // TimerInterrupt                                                             // https://github.com/pcbreflux/espressif/blob/master/esp32/arduino/sketchbook/ESP32_simpletimer/ESP32_simpletimer.ino
   Serial.println("Start Timer");                                        // Debug
   //Serial.println(interruptdelay);
   timer = timerBegin(0, 80, true);                                      // MWDT clock period = 12.5 ns * TIMGn_Tx_WDT_CLK_PRESCALE -> 12.5 ns * 80 -> 1000 ns = 1 us, countUp
@@ -92,39 +113,41 @@ void setup()
 }
 
 
-//8==========D
 void loop()
 {
   Serial.println("Interrupt Loop");
-
   if (interruptflag)
   {
-    ringBuffer.push(analogRead(ADC_PIN));     // Read analog signal and write to buffer
-    interruptflag = false;                    // Reset interrupt flag
+    
   }
 
 
   txval = (int) 127*(1+sin(2*PI*millis()/1000)); // sin ist im Bereich [0;2]. Mit *127 wird gesamter 8bit Bereich genutzt.
   dacWrite(DAC_PIN, txval);
-  rxval = analogRead(ADC_PIN);
+  
 
-  rxvoltage = float(rxval)/4095*3.3; //ADC ist 12bit,
-
-  if (xdisplay == 128)
+  if (displaypx.x == 128)             // Reset x coordinate to start of the display
   {
     display.clear();
-    xdisplay = 0;
-    ydisplay = 0;
+    lastpx.x = 0;
+    displaypx.x = 0;
   }
   
-  ydisplay = rxvoltage/3.3 * 64;
-
-  if (delayvalue >= 8)
+  if (delayvalue >= 5)      // Display every xth value
   {
-    display.setPixel(xdisplay, ydisplay);
-    display.display();
-    xdisplay++;
+    rxval = analogRead(ADC_PIN);
 
+    rxvoltage = float(rxval)/4095*3.3; //ADC ist 12bit,
+
+    displaypx.y = rxvoltage/3.3 * 64;   // Convert read data into display space
+
+    display.setPixel(displaypx.x, displaypx.y);
+    display.drawLine(displaypx.x, displaypx.y, lastpx.x, lastpx.y);  // Connect last and current pixel
+    display.display();
+    
+    lastpx = displaypx;     // Save last coordinates for line drawing
+    
+    displaypx.x++;
     delayvalue = 0;
   }
   delayvalue++;
