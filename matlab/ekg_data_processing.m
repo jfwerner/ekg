3a2a5e2cc4a16bf80ea5f24ef481cd0b7d4a795a
jojo313 <32206115+jojo313@users.noreply.github.com>
2023-11-09 16:55:24 +0100

    Filter und Filterkoeffizienttenberechnung EKG Sample Daten Verbesserung Sendefunktion
    Filterung und doppeltes Senden
    
    Co-authored-by: tamasm3 <tamasm3@users.noreply.github.com>

--- matlab/ekg_data_processing.m
+++ matlab/ekg_data_processing.m
@@ -12,24 +12,29 @@ clc
 %% Laden des EGK-Signals
 
 %load("EKG-Daten_tamara3.mat");
-load("EKG-Daten_johannes_2.mat");
+load("EKG-Daten_raw_johannes_4.mat");
+load("EKG-Daten_IIR_johannes_4.mat");
+%load("EKG-Daten_johannes_2.mat");
+
+%ekg_data_raw = ekg_data;
 
 %% Allgemeine Parameter
 
-N = length(ekg_data);
+N = length(ekg_data_raw);
 fA = 250;                           % Abtastfrequenz in Hz
 TA = 1/fA;                          % Abtastzeit in s
-T = 30;                             % Simulationszeit in Sekunde
+%T = 30;                             % Simulationszeit in Sekunde
 t = linspace(0, (N-1) * TA, N);     % Zeitvektor 
-% t = linspace(0, T, N);            % Zeitvektor für 30s
 
 FreqAxis = linspace(0, fA, N+1);    % Frequenzachse
 FreqAxis = FreqAxis(1:end-1);                   
 
 
-Fw = fft(ekg_data);                             % Fouriertransformation - Betragspektrum
+Fw_raw = fft(ekg_data_raw);                         % Fouriertransformation - Betragspektrum
+Fw_IIR = fft(ekg_data_IIR);
+Fw_raw_A = abs(Fw_raw) / N;                             % Amplitudenspektrum
+Fw_IIR_A = abs(Fw_IIR) / N;
 
-Fw_A = abs(Fw) / N;                             % Amplitudenspektrum
 
 % Index des Koeffizienten bei 49,5 Hz und 50,5 Hz & (199,5 Hz und 200,5 Hz)
 index_49_5Hz = round(49.5 / (fA / N));
@@ -40,48 +45,65 @@ index_200_5Hz = round(200.5  / (fA / N));
 
 
 % Frequenzbereich zwischen 49,5 Hz und 50,5 Hz (& 199,5 Hz und 200,5 Hz) eliminieren
-Fw_filt = Fw;
+Fw_filt = Fw_raw;
 Fw_filt(index_49_5Hz:index_50_5Hz) = 0;
 Fw_filt(index_199_5Hz:index_200_5Hz) = 0;
 
 
-ekg_data_f = ifft(Fw_filt);                     % Inverse FFT, um das gefilterte Signal zurückzubekommen
-Fw_filt_A = abs(Fw_filt) / N;                   % Amplitudenspektrum
+ekg_data_filt = ifft(Fw_filt);                      % Inverse FFT, um das gefilterte Signal zurückzubekommen
+Fw_filt_A = abs(Fw_filt) / N;                       % Amplitudenspektrum
 
 
 %% Plot der EKG-Aufnahme
 figure(1)
-subplot(2, 1, 1)
-plot(t, ekg_data);
+subplot(3, 1, 1)
+plot(t, ekg_data_raw);
 xlabel('Zeit in Sekunde');
 ylabel('Spannung in Volt');
 title('EKG-Signal', 'Rohdaten');
 axis([0, 30, 0, 3.3])
 grid;
 
-subplot(2, 1, 2)
-plot(t, ekg_data_f);
-title('', 'Gefiltert');
+subplot(3, 1, 2)
+plot(t, ekg_data_filt);
+title('', 'Gefiltert in Matlab');
 xlabel('Zeit in Sekunde');
 ylabel('Spannung in Volt');
 axis([0, 30, 0, 3.3])
 grid;
 
+subplot(3, 1, 3)
+plot(t, ekg_data_IIR);
+title('', 'Gefiltert mit digitalem IIR-Notch-Filter');
+xlabel('Zeit in Sekunde');
+ylabel('Spannung in Volt');
+axis([0, 30, 0, 3.3])
+grid;
 
 %% Plot des Amplitudenspektrums
 figure(2)
-subplot(2, 1, 1)
-stem(FreqAxis, Fw_A);
+subplot(3, 1, 1)
+stem(FreqAxis, Fw_raw_A);
 title('Amplitudenspektrum des Signals', 'Rohdaten');
-xlabel('Frequenz in Hertz)');
-ylabel('Amplitude');
-grid;
-
-subplot(2, 1, 2)
-stem(FreqAxis, Fw_filt_A);
-title('','Gefiltert');
 xlabel('Frequenz in Hertz');
 ylabel('Amplitude');
+xlim([0, 55]);
+grid;
+
+subplot(3, 1, 2)
+stem(FreqAxis, Fw_filt_A);
+title('','Gefiltert in Matlab');
+xlabel('Frequenz in Hertz');
+ylabel('Amplitude');
+xlim([0, 55]);
+grid;
+
+subplot(3, 1, 3)
+stem(FreqAxis, Fw_IIR_A);
+title('','Gefiltert mit digitalem IIR-Notch-Filter');
+xlabel('Frequenz in Hertz');
+ylabel('Amplitude');
+xlim([0, 55]);
 grid;
 
 %%  Herzratenvariabilität (HRV) 
@@ -93,9 +115,9 @@ diffBetweenHB = 0;                  % Vektor mit Zeitdifferenz zw. R-Zacken | RR
 n = 1;                              % Index äußere while-Schleife
 i = 1;                              % Index innere while-Schleife
 
-
 figure(3)
-plot(t, ekg_data);
+% subplot (2, 1, 1)
+plot(t, ekg_data_raw);
 xlabel('Zeit in Sekunde');
 ylabel('Spannung in Volt');
 title('EKG-Signal mit R-Zacken-Detektion');
@@ -103,9 +125,9 @@ axis([0, 30, 0, 3.3])
 hold on
 grid;
 
-while (n < length(ekg_data))                   % Go through all data
-    if (ekg_data(1,n) >= Rthreshold)           % Look for QRS-Komplexe (R-Zacke)
-        while (ekg_data(1,n) >= Rthreshold)    % Look for end of QRS
+while (n < length(ekg_data_raw))                   % Go through all data
+    if (ekg_data_raw(1,n) >= Rthreshold)           % Look for QRS-Komplexe (R-Zacke)
+        while (ekg_data_raw(1,n) >= Rthreshold)    % Look for end of QRS
             n=n+1;
         end
         HBcount = HBcount + 1;                 % Count heartbeat
@@ -121,5 +143,9 @@ bpm = HBcount / 30 * 60;                        % Herzschläge pro Minute
 hrv = std(timeSinceLastHB);                     % Herzratenvariabilität
 hold off;
 
+% EKG to C++ array
+% ekg_data_raw=ekg_data_raw/3.3*4095
+% fmt=['uint%d_t %s={' repmat('%d,',1,numel(ekg_data_raw)-1) '%d}'];
+% c_code=sprintf(fmt,8,'ekg_data',ekg_data_raw)
 
 
--- matlab/ekg_updport.m
+++ matlab/ekg_updport.m
@@ -11,50 +11,60 @@ clc
 
 %% Initialisierung
 u = udpport("IPV4")                                             % Construct a byte-type udpport object.
-ekg_data = [];                                                  % Vektor fur EKG Daten
+ekg_data_raw = [];                                              % Vektor fur raw-EKG Daten
+ekg_data_IIR = [];                                              % Vektor fur geilterte EKG Daten
 packet_number = 1;
 
-% Aufnahmezeit. Ein Paket entspricht 15 Sekunden Daten.
-packets = 2;
+packets = 4;                                                        % Ein Paket entspricht 15 Sekunden Daten.
 
-%writeline(u, "Bereit zu empfangen", "192.168.188.151", 420);    % Nachricht an ESP32 (IP-Adresse und den Port)
-writeline(u, "Bereit zu empfangen", "192.168.0.60", 420);
+writeline(u, "Bereit zu empfangen", "192.168.188.151", 420);   % Nachricht an ESP32 (IP-Adresse und den Port)
+%writeline(u, "Bereit zu empfangen", "192.168.0.60", 420);
 
-
-while (packet_number <= packets)                                % Solange Auslesen bis mindestens 7500 Werte gelesen wurden (30s)
+while (packet_number <= packets)                                % Solange Auslesen bis gewollte Paketanzahl erreicht
     
     while (u.NumBytesAvailable == 0)
     disp('I´m just chilling here');
     disp(packet_number);
     end
 
-    %packet_size = u.NumBytesAvailable/2;
     disp(u.NumBytesAvailable)
-
-    data_packet = read(u, 3750, "uint16");                      % ESP sendet 1 Byte (8bit) Daten
     
-    ekg_data = [ekg_data, data_packet];                         % Die empfangenen Daten an den EKG-Datenvektor anhängen
-   
-    %end
+    if mod(packet_number, 2) == 0                               % packetnumber ist gerade
+        data_packet = read(u, 3750, "uint16");                  % ESP sendet 1 Byte (8bit) Daten
+        ekg_data_IIR = [ekg_data_IIR, data_packet];             % Die empfangenen Daten an den EKG-Datenvektor anhängen
+    else                                                        % ungerade
+        data_packet = read(u, 3750, "uint16");                  % ESP sendet 1 Byte (8bit) Daten
+        ekg_data_raw = [ekg_data_raw, data_packet];             % Die empfangenen Daten an den EKG-Datenvektor anhängen
+    end
 
     packet_number = packet_number + 1;
 
 end
 
-
-
 % Daten anzeigen                    
 % disp(ekg_data);
 % disp(data);                                             
 
-t=linspace(0,15*packets,length(ekg_data));
-ekg_data=ekg_data/4095*3.3;                                     % ADC-Werte in Spannung umrechnen
-plot(t,ekg_data);
+t=linspace(0,15*packets/2,length(ekg_data_raw));
+ekg_data_raw=ekg_data_raw/4095*3.3;                            % ADC-Werte in Spannung umrechnen
+subplot(2,1,1)
+plot(t, ekg_data_raw);
 xlabel("Zeit (s)");
 ylabel("Spannung (V)");
-title("EKG Daten ESP32");
+title('Aufgenomme EKG-Daten', 'Rohdaten');
 axis([0, 30, 0, 3.3])
-save("EKG-Daten_johannes_2.mat","ekg_data");                  % Die gelesenen Daten werden abgespeichert
+
+subplot(2,1,2)
+ekg_data_IIR=ekg_data_IIR/4095*3.3;                            % ADC-Werte in Spannung umrechnen
+plot(t, ekg_data_IIR);
+xlabel("Zeit (s)");
+ylabel("Spannung (V)");
+title('', 'Gefilterte Daten mit IIR-Notch-Filter');
+axis([0, 30, 0, 3.3])
+
+save("EKG-Daten_raw_johannes_4.mat","ekg_data_raw");           % Die gelesenen Daten werden abgespeichert
+save("EKG-Daten_IIR_johannes_4.mat","ekg_data_IIR");           % Die gelesenen Daten werden abgespeichert
+
 
 flush(u, "output");                                             % Den Ausgabepuffer leeren 
 clear u;                                                        % Das udpport-Objekt löschen
